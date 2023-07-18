74456a9fc1e53d7f83d65d8b13f4832999f46ff6
Tim Jacomb <timjacomb1@gmail.com>
2023-07-18 11:19:01 +0100

    DTSPO-14238 Initial module

--- inputs-optional.tf
+++ inputs-optional.tf
@@ -1,7 +1,6 @@
-variable "existing_resource_group_name" {
+variable "resource_group_name" {
   description = "Name of existing resource group to deploy resources into"
   type        = string
-  default     = null
 }
 
 variable "location" {
@@ -11,6 +10,22 @@ variable "location" {
 }
 
 variable "name" {
-  default     = ""
-  description = "The default name will be product+component+env, you can override the product+component part by setting this"
+  description = "The default name will be product+env, you can override the product part by setting this"
+  default     = null
+  type        = string
+}
+
+variable "application_type" {
+  default     = "web" # most of our instances have historically used web, even if other might be more appropriate
+  description = "Specifies the type of Application Insights to create. Valid values are `java` for Java web, `Node.JS` for Node.js, `other` for General, and `web` for ASP.NET"
+}
+
+variable "sampling_percentage" {
+  default     = 100
+  description = "Specifies the percentage of the data produced by the monitored application that is sampled for Application Insights telemetry."
+}
+
+variable "daily_data_cap_in_gb" {
+  default     = 100
+  description = "Specifies the Application Insights component daily data volume cap in GB"
 }
