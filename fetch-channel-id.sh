725ae6af678d4ba4ad150666bab3932806b1b030
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-02-08 10:58:12 +0000

    test - bash script

--- fetch-channel-id.sh
+++ fetch-channel-id.sh
@@ -9,17 +9,17 @@ cnp_json=$(curl -s https://raw.githubusercontent.com/hmcts/cnp-jenkins-config/ma
 
 echo -n "{\"channel_id\":\"C8SR5CAMU\"}"
 
-# channel_id=$(echo "$cnp_json" | jq --arg PRODUCT "$PRODUCT" -r '.[$PRODUCT] | .slack.channel_id')
+channel_id=$(echo "$cnp_json" | jq --arg PRODUCT "$PRODUCT" -r '.[$PRODUCT] | .slack.channel_id')
 
 
-# if [ "$channel_id" = "null" ]; then
-#   sds_json=$(curl -s https://raw.githubusercontent.com/hmcts/sds-jenkins-config/master/team-config.yml | yq e -o=json)
-#   channel_id=$(echo "$sds_json" | jq --arg PRODUCT "$PRODUCT" -r '.[$PRODUCT] | .slack.channel_id')
+if [ "$channel_id" = "null" ]; then
+  sds_json=$(curl -s https://raw.githubusercontent.com/hmcts/sds-jenkins-config/master/team-config.yml | yq e -o=json)
+  channel_id=$(echo "$sds_json" | jq --arg PRODUCT "$PRODUCT" -r '.[$PRODUCT] | .slack.channel_id')
   
-#   if [ "$channel_id" = "null" ]; then
-#     channel_id="NOT FOUND"
-#   fi
-#   # echo $channel_id
-# fi
+  if [ "$channel_id" = "null" ]; then
+    channel_id="NOT FOUND"
+  fi
+  # echo $channel_id
+fi
 
-# echo -n "{\"channel_id\":\"$channel_id\"}"
+echo -n "{\"channel_id\":\"$channel_id\"}"
