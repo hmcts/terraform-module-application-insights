1d8e167e7095c7d25075f08f1ec3307e41c240e8
Zoe Cope <zoe.cope@hmcts.net>
2024-02-14 15:38:15 +0000

    remove provider configurations

--- init.tf
+++ init.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
   skip_provider_registration = true
