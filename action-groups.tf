4a098a213e9d6c4f7a12cf242909e14818d86da1
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-02-14 12:49:26 +0000

    Fix env issue on CFT

--- action-groups.tf
+++ action-groups.tf
@@ -1,7 +1,8 @@
 locals {
   env = (var.env == "aat") ? "stg" : (var.env == "sandbox") ? "sbox" : "${(var.env == "perftest") ? "test" : "${var.env}"}"
 
-  rg_env        = (var.env == "stg" && strcontains(lower(data.azurerm_subscription.current.display_name), "cftapps")) ? "aat" : (var.env == "test" && strcontains(lower(data.azurerm_subscription.current.display_name), "cftapps")) ? "perftest" : var.env == "sandbox" ? "sbox" : var.env
+  rg_env = (var.env == "stg" && strcontains(lower(data.azurerm_subscription.current.display_name), "cftapps")) ? "aat" : (var.env == "test" && strcontains(lower(data.azurerm_subscription.current.display_name), "cftapps")) ? "perftest" : var.env == "sandbox" ? "sbox" : var.env
+
   business_area = strcontains(lower(data.azurerm_subscription.current.display_name), "cnp") || strcontains(lower(data.azurerm_subscription.current.display_name), "cftapps") ? "cft" : "sds"
 }
 
