a72c820f86a721880d0151b59e974828cd218ffc
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-02-16 15:57:44 +0000

    restored for cft

--- init.tf
+++ init.tf
@@ -11,6 +11,5 @@ provider "azurerm" {
   features {}
   skip_provider_registration = true
   alias                      = "ptl_subscription"
-  subscription_id            = "6c4d2513-a873-41b4-afdd-b05a33206631"
-  # subscription_id            =  local.business_area == "sds" ? "6c4d2513-a873-41b4-afdd-b05a33206631" : "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+  subscription_id            = local.business_area == "sds" ? "6c4d2513-a873-41b4-afdd-b05a33206631" : "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
 }
