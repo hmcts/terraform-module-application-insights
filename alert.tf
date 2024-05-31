2587009614e61a3008d4caa830fb7f982f5b588e
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-05-31 13:59:19 +0100

    Setting threshold to equals 0 for testing

--- alert.tf
+++ alert.tf
@@ -56,7 +56,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "main" {
         | where OperationNameValue == "Microsoft.Insights/Components/DailyCapReached/Action"
       QUERY
       time_aggregation_method = "Count"
-      operator                = "GreaterThan"
+      operator                = "Equal"
       threshold               = 0
   }
 
