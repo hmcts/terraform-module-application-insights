37a212f16fb645e972674352caf8bfa604ce8faa
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-06-10 10:28:16 +0100

    Change trigger for testing

--- alert.tf
+++ alert.tf
@@ -66,7 +66,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "main" {
           | where Category == "Administrative"
         QUERY
     time_aggregation_method = "Count"
-    operator                = "Equals"
+    operator                = "Equal"
     threshold               = 0
   }
 
