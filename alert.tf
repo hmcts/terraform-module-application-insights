fbe661ff22d0431ec38ff739e76dd71ff3eb5379
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-06-10 12:30:08 +0100

    Add failing_periods block

--- alert.tf
+++ alert.tf
@@ -62,6 +62,11 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "main" {
     time_aggregation_method = "Count"
     operator                = "GreaterThan"
     threshold               = 0
+
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = 1
+      number_of_evaluation_periods             = 1
+    }
   }
 
   action {
