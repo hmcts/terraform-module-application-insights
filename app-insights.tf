e6964cd416a83c44747838bbabd2e5947c80bfd0
Nicholas Chang <nicholas.chang@justice.gov.uk>
2025-12-10 14:35:25 +0000

    edit variable for app insights sampling

--- app-insights.tf
+++ app-insights.tf
@@ -18,6 +18,7 @@ resource "azurerm_application_insights" "this" {
 
   location            = var.location
   resource_group_name = var.resource_group_name
+
   application_type     = var.application_type
   daily_data_cap_in_gb = var.daily_data_cap_in_gb
   sampling_percentage  = local.sampling_percentage
